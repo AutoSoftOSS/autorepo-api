fd42f9881ad0e66b4fbd4ceaa89b34be1ca1b504
Connor White <connor.bcw@gmail.com>
2023-01-06 21:21:35 -0600

    fix: update to new gitkraken-cli

--- source/commands/open/index.ts
+++ source/commands/open/index.ts
@@ -2,7 +2,7 @@ import clee from "clee";
 import { openRepo } from "./repo/index.js";
 import { openNPM } from "./npm.js";
 import { openEditor } from "./editor.js";
-import { openKraken } from "./kraken.js";
+import openKraken from "gitkraken-cli";
 
 export const open = clee("open")
   .command(openRepo)
--- source/commands/open/kraken.ts
+++
@@ -1,6 +0,0 @@
-import clee from "clee";
-import { openAction } from "gitkraken-cli";
-
-export const openKraken = clee("kraken")
-  .description("Open the current repository in GitKraken")
-  .action(openAction);
