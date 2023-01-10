453d18127fe478e0478f390fc2d9221e53602b14
Connor White <connor.bcw@gmail.com>
2023-01-10 10:24:57 -0600

    fix: check if package exists before importing

--- source/commands/import.ts
+++ source/commands/import.ts
@@ -8,11 +8,14 @@ export const importPackage = clee("import")
   .argument("<url>", "Repo URL to import from")
   .option("-n", "--name", "[name]", "Package directory name", parseString)
   .action(async (url, { name }) => {
-    const { repo: repoName } = parseRepositoryURL(url) ?? {};
-    const directoryName = name ?? repoName;
+    const directoryName = name ?? parseRepositoryURL(url)?.repo;
     if(directoryName) {
       const pkgDir = structure().files().packages.subdirectory(directoryName);
-      await exec("git", ["submodule", "add", url, pkgDir.relative]);
+      if(await pkgDir.exists()) {
+        throw new Error("Package already exists.");
+      } else {
+        await exec("git", ["submodule", "add", url, pkgDir.relative]);
+      }
     } else {
       throw new Error("Could not determine package name from URL");
     }
